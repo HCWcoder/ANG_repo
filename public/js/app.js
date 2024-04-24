7230e6aeeeedf3fb639c1df3b13b83eb138a994b
HCWcoder <jpwwaked@gmail.com>
2024-04-24 22:40:13 +0300

    v3

---
+++ public/js/app.js
@@ -0,0 +1,196 @@
+
+    const songs = [
+        {id: "1122520672", name: "هلالالا"},
+        {id: "113354", name: "ورينى"},
+        {id: "5101858", name: "اجازة (مع بوسي)"},
+        {id: "1084162841", name: "أنا بيروت"},
+        {id: "1044059663", name: "دكتور في الحب"},
+        {id: "44340737", name: "ما فيش كده"},
+        {id: "113154", name: "بناديلك"},
+        {id: "110399977", name: "ليالي الصيف"},
+        {id: "30641982", name: "برّا"},
+        {id: "100375988", name: "ولهان"},
+        {id: "93537936", name: "ماشي مع جمال ياسين ورامي شلهوب"},
+        {id: "112976", name: "قوللى إزاي"},
+        {id: "1056005819", name: "بمبي"},
+        {id: "98429417", name: "ماشي ريمكس (مع جمال ياسين & رامي شلهوب)"},
+        {id: "115014", name: "كسرتلى السيارة"},
+        {id: "1046312345", name: "عيشها بمزاج"},
+        {id: "55513868", name: "مين قلك"},
+        {id: "1074013011", name: "موج"},
+        {id: "55513905", name: "أقولك إيه"},
+        {id: "5252117", name: "اجازة (مع بوسي) [ريمكس جيمي حداد]"}
+    ];
+
+    function toggleSongList() {
+        const listContainer = document.getElementById('songList');
+        if (listContainer.style.display === 'none' || listContainer.style.display === '') {
+            listContainer.style.display = 'block';
+            populateSongList();
+        } else {
+            listContainer.style.display = 'none';
+        }
+    };
+
+    function populateSongList() {
+        const listContainer = document.getElementById('songList');
+        listContainer.innerHTML = ''; // Clear existing list
+        songs.forEach(song => {
+            const div = document.createElement('div');
+            div.className = 'song-item';
+            div.textContent = `${song.name} (${song.id})`;
+            div.onclick = function() {
+                document.getElementById('songId').value = song.id;
+                toggleSongList(); // Hide the list after selection
+            };
+            listContainer.appendChild(div);
+        });
+    };
+    document.addEventListener('DOMContentLoaded', function() {
+        let wasBusy = false;
+        fetch('/get-order-history')
+            .then(response => response.json())
+            .then(orders => {
+            document.getElementById('historyCount').innerText = `[${orders.length}]`;
+            orders.sort((a, b) => b.id - a.id);
+            const tableBody = document.querySelector('table tbody');
+            orders.forEach(order => {
+                const row = document.createElement('tr');
+                let statusColor = '';
+                switch (order.status) {
+                    case 'Completed':
+                        statusColor = 'status-completed';
+                        break;
+                    case 'Error':
+                        statusColor = 'status-completed';
+                        break;
+                    case 'Pending':
+                        statusColor = 'status-pending';
+                        break;
+                }
+                row.innerHTML = `
+                    <td>
+                        <a href="https://play.anghami.com/song/${order.songId}" target="_blank" title="https://play.anghami.com/song/${order.songId}">
+                            ${order.songId}
+                        </a>
+                    </td>
+                    <td>${order.plays}</td>
+                    <td>${order.country}</td>
+                    <td>${order.action || 'Play'}</td>
+                    <td class="${statusColor}">${order.status}</td>
+                `;
+                tableBody.appendChild(row);
+                });
+            });
+        const checkServerBusy = () => {
+            fetch('/check-busy')
+                .then(response => response.json())
+                .then(data => {
+                    const submitButton = document.querySelector('#voteForm button');
+                    const loader = document.querySelector('.loader');
+                    submitButton.disabled = data.isBusy;
+                    loader.style.display = data.isBusy ? 'block' : 'none'; // Show or hide the loader
+                    if (wasBusy && !data.isBusy) {
+                        window.location.reload();  // Reload the page once when loader disappears
+                    }
+                    wasBusy = data.isBusy;
+                })
+                .catch(error => console.error('Failed to check server status:', error));
+        };
+
+        checkServerBusy();
+        setInterval(checkServerBusy, 5000);
+    });
+    document.getElementById('voteForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission
+        var songId = document.getElementById('songId').value;
+        if (!songId) {
+            alert('Please enter a Song ID before submitting.');
+            return; // Stop the form submission
+        }
+        console.log('submitting form...');
+        const actionUrl = event.submitter.formAction || this.action; // Determine the action based on the clicked button
+        console.log(actionUrl);
+        const formData = new FormData(this);
+        var playsText = document.getElementById('playsValue').innerText;
+        // Extract the number from the text, assuming it follows a format like "+50 [3.00 minutes]"
+        var totalMinutes = parseInt(playsText.match(/[\d\.]+/)[0], 10);
+        const minutesPerThousand = 6; // Assume each 1000 plays takes 6 minutes
+        var totalMinute = (totalMinutes / 1000) * minutesPerThousand;
+        console.log('totalMinutes', totalMinute);
+        if (!isNaN(totalMinute)) { // Check if the conversion result is a number
+            setTimeout(function() {
+                startTimer(totalMinute * 60); // Convert minutes to seconds for the timer
+            }, 4000); // 3000 milliseconds = 3 seconds
+        } else {
+            console.error("Failed to extract total minutes from text:", playsText);
+        }
+        fetch(actionUrl, {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.text())
+        .then(data => {
+            window.location.reload();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+    function updatePlaysValue(value) {
+        const minutesPerThousand = 6; // Assume each 1000 plays takes 6 minutes
+        var totalMinutes = (value / 1000) * minutesPerThousand;
+        document.getElementById('playsValue').innerText = '+' + value + ' [' + totalMinutes.toFixed(2) + ' minutes]';
+    }
+    
+    function startTimer(duration) {
+        var timer = duration, minutes, seconds;
+        const display = document.getElementById('timerDisplay');
+        display.style.display = 'block'; // Show the timer when starting
+        clearInterval(window.countdown); // Clear existing timer if any
+        window.countdown = setInterval(function () {
+            minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+    
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+            display.textContent = minutes + ":" + seconds; // Update the timer display
+    
+            if (--timer < 0) {
+                clearInterval(window.countdown);
+                display.textContent = "00:00"; // Reset timer when it reaches 0
+                setTimeout(function() {
+                    display.style.display = 'none'; // Hide the timer
+                }, 3000); // 3000 milliseconds = 3 seconds
+            }
+        }, 1000);
+    }
+    document.addEventListener('DOMContentLoaded', function() {
+        fetch('/get-unique-song-details')
+            .then(response => response.json())
+            .then(songDetails => {
+                const banner = document.getElementById('songIdBanner');
+                songDetails.forEach(detail => {
+                    const span = document.createElement('span');
+                    span.textContent = `${detail.songId}`;
+                    span.style.margin = '5px';
+                    span.className = 'spanses';
+                    span.style.padding = '5px';
+                    span.style.border = '1px solid gray';
+                    span.style.cursor = 'pointer';
+                    span.onclick = function() { fillInputsWithDetails(detail); };
+                    banner.appendChild(span);
+                });
+            })
+            .catch(error => console.error('Error loading song details:', error));
+    });
+
+    function fillInputsWithDetails(detail) {
+        const songIdInput = document.querySelector('input[name="songId"]');
+        const playsInput = document.querySelector('input[name="plays"]');
+
+        songIdInput.value = detail.songId;
+        updatePlaysValue(detail.plays);
+};
+
---
+++ server/appv2.js
@@ -0,0 +1,138 @@
+const http = require('http');
+const serveStatic = require('serve-static');
+const finalhandler = require('finalhandler');
+const path = require('path');
+const fs = require('fs');
+const formidable = require('formidable');
+const { spawn } = require('child_process');
+
+// Directory paths
+const publicDir = path.join(__dirname, '../public');
+const dataDir = path.join(__dirname, '../data');
+const orderHistoryFile = path.join(dataDir, 'orderHistory.json');
+let isBusy = false;
+let orderHistory = [];
+
+// Save order history to file
+function saveOrderHistory() {
+    fs.writeFile(orderHistoryFile, JSON.stringify(orderHistory, null, 2), err => {
+        if (err) console.error('Error writing file:', err);
+    });
+}
+
+// Handle sending vote with a Python script
+function handleVote(param, fields, res) {
+    const paramsMap = {'/send-vote': '-p', '/send-vote2': '-l', '/send-vote3': '-f'};
+    const actionMap = {'-p': 'Play', '-l': 'Like', '-f': 'Follow'};
+    if (paramsMap[param] && !isBusy) {
+        param = paramsMap[param];
+    } else {
+        res.end('Server is busy sending votes... Please try again later.');
+    }
+    const { country, songId, plays } = fields;
+    const songIdSplit = songId[0].toString().split('/');
+    const songIdFinal = songIdSplit[songIdSplit.length - 1];
+
+    const actionType = actionMap[param] || 'Unknown Action';
+    const pythonScript = 'python3';
+    const args = [
+        'send_vote.py',
+        param, songIdFinal,
+        '-v', plays[0],
+        '-c', 'EG',
+        '-t', '35',
+        '--old_tokens'
+    ];
+
+    isBusy = true;
+    orderHistory.push({
+        id: orderHistory.length + 1,
+        songId: songIdFinal,
+        plays: plays[0],
+        country: country[0],
+        action: actionType,
+        status: 'Pending'
+    });
+    saveOrderHistory();
+
+    const pythonProcess = spawn(pythonScript, args);
+    pythonProcess.stdout.on('data', data => console.log(`send_vote.py output: ${data}`));
+    pythonProcess.stderr.on('data', data => console.error(`send_vote.py error: ${data}`));
+    pythonProcess.on('close', code => {
+        isBusy = false;
+        orderHistory[orderHistory.length - 1].status = code === 0 ? 'Completed' : 'Completed';
+        saveOrderHistory();
+
+        res.writeHead(code === 0 ? 200 : 200);
+        res.end(code === 0 ? 'Vote sent successfully' : 'Vote sent successfully');
+    });
+}
+
+const serve = serveStatic(publicDir, {
+    'index': ['index.html', 'index.htm']
+});
+
+// Create HTTP server
+const server = http.createServer((req, res) => {
+    const done = finalhandler(req, res);
+    serve(req, res, () => {
+        const url = new URL(req.url, `http://${req.headers.host}`);
+        console.log(url.pathname);
+        switch (url.pathname) {
+            case '/':
+                fs.readFile('index.html', (err, data) => {
+                    if (err) {
+                        res.writeHead(500);
+                        res.end("Error loading index.html");
+                    } else {
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        res.end(data);
+                    }
+                });
+                break;
+            case '/send-vote':
+            case '/send-vote2':
+            case '/send-vote3':
+                if (!isBusy) {
+                    console.log("post on ", url.pathname);
+                    const form = new formidable.IncomingForm();
+                    form.parse(req, (err, fields) => {
+                        if (err) {
+                            console.error(err);
+                            res.writeHead(500);
+                            res.end("Error parsing the form data");
+                        } else {
+                            handleVote(url.pathname, fields, res);
+                        }
+                    });
+                } else {
+                    res.end('Server is currently busy. Please try again later.');
+                }
+                break;
+            case '/get-order-history':
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(orderHistory));
+                break;
+            case '/get-unique-song-details':
+                const songDetails = orderHistory.reduce((acc, order) => ({
+                    ...acc,
+                    [order.songId]: { songId: order.songId, plays: order.plays }
+                }), {});
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(Object.values(songDetails)));
+                break;
+            case '/check-busy':
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ isBusy }));
+                break;
+            default:
+                done();  // Let finalhandler manage not found errors
+                break;
+        }
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    orderHistory = fs.readFile(orderHistoryFile, (err, data) => err ? console.error('Error reading file:', err) : orderHistory = JSON.parse(data));
+});
